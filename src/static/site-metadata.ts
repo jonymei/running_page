58dd631463d9de4cd41cd47f56df9369e60444e9
jonymei <ahumeijun@live.com>
2024-05-13 18:34:18 +0800

    自定义 metadata

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -10,14 +10,14 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
+  siteTitle: 'May\'s Running Page',
+  siteUrl: 'https://run.maymagic.us',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
   navLinks: [
     {
       name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
+      url: 'https://blog.maymagic.us',
     },
     {
       name: 'About',
